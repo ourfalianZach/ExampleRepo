d05a1185c68e85202aaf9042ae52eec2bcbf00b6
ZacharyOurfalia <>
2022-05-25 17:17:43 +0000

    message

---
+++ main.py
@@ -0,0 +1,10 @@
+from pytube import YouTube as yt
+
+video = input("Videa URL: ")
+d = yt(video)
+
+dl = d.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1]
+
+dl.download('./')
+
+print('download complete')
